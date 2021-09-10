3189c375939cd46d56849940981d4b8e7d3cbee2
Jeremy <jeremy.dufeux@gmail.com>
2021-09-10 11:42:44 +0200

    Upgrade dependencies

---
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/database/PropertyDaoFilterTest.kt
@@ -0,0 +1,44 @@
+package com.openclassrooms.realestatemanager.database
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class PropertyDaoFilterTest{
+
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var propertyDao: PropertyDao
+
+
+    @Before
+    fun inject(){
+        hiltRule.inject()
+    }
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        mainThreadSurrogate.close()
+    }
+}
