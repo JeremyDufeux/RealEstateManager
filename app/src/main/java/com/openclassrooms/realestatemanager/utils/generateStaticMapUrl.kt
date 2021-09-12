b218c9e10592cc06e8f95a6f514523aea72511da
Jeremy <jeremy.dufeux@gmail.com>
2021-09-12 08:53:27 +0200

    Add getStaticMapUrl tests

--- app/src/main/java/com/openclassrooms/realestatemanager/utils/generateStaticMapUrl.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/utils/generateStaticMapUrl.kt
@@ -5,8 +5,8 @@ import com.openclassrooms.realestatemanager.BuildConfig
 fun getStaticMapUrl(latitude : Double?, longitude : Double?) : String? {
     val baseUrl = "https://maps.googleapis.com/maps/api/staticmap" +
             "?center=%s,%s" +
-            "&zoom=15&" +
-            "size=600x400" +
+            "&zoom=15" +
+            "&size=600x400" +
             "&maptype=roadmap" +
             "&markers=color:0xff4081|%s,%s" +
             "&key=%s"
